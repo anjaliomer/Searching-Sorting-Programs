dad7cc4c3fe5d864bd914e04a15811a89d034357
anjaliomer <anjaliom@buffalo.edu>
2018-05-03 11:22:52 -0400

    Added Sorted Rotated Array

---
+++ Sorted Rotated Array/Sorted Rotated Array/sorted_rotated_array.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "sorted_rotated_array.h"
+
+int sorted_rotated_array(int ar[], int size, int num)
+{
+	int hi = find_pivot(ar, size);
+	int first;
+	int last;
+
+	if (num >= ar[hi + 1] && num <= ar[size - 1])
+	{
+		first = hi + 1;
+		last = size - 1;
+		return binary_search(ar, first, last, num);
+	}
+	else if (num >= ar[0] && num <= ar[hi])
+	{
+		first = 0;
+		last = hi;
+		return binary_search(ar, first, last, num);
+	}
+	else
+		return -1;
+}
+
+int find_pivot(int ar[], int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		int diff = ar[i] - ar[i - 1];
+		if (diff < 0)
+		{
+			return (i - 1);
+		}
+	}
+}
+
+int binary_search(int ar[], int first, int last, int num)
+{
+	while (first <= last)
+	{
+		int mid = first + (last - first) / 2;
+		if (ar[mid] == num)
+			return mid;
+		else if (ar[mid] < num)
+			first = mid + 1;
+		else if (ar[mid] > num)
+			last = mid - 1;
+	}
+	return -1;
+}
---
+++ Sorted Rotated Array/Sorted Rotated Array/sorted_rotated_array.h
@@ -0,0 +1,3 @@
+int sorted_rotated_array(int[], int, int);
+int find_pivot(int[], int);
+int binary_search(int[], int, int, int);
