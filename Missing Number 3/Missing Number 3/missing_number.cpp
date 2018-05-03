9ec41a9a8ebd2a8263140eff78258a02d51ed4a5
anjaliomer <anjaliom@buffalo.edu>
2018-05-03 11:18:06 -0400

    Added Missing Number 3

---
+++ Missing Number 3/Missing Number 3/missing_number.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "missing_number.h"
+
+int missing_number(int ar[], int size)
+{
+	int x1 = ar[0];
+
+	for (int i = 1; i<(size - 1);i++)
+	{
+		x1 = x1 ^ ar[i];
+	}
+
+	int x2 = 1;
+
+	for (int i = 2; i <= size; i++)
+	{
+		x2 = x2 ^ i;
+	}
+
+	return x1 ^ x2;
+}
