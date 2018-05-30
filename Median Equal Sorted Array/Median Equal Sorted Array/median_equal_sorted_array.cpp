7e8a48a081b662f8535c745db60c80e72e699274
anjaliomer <anjaliom@buffalo.edu>
2018-05-30 15:49:13 -0400

    Added Median Equal Sorted Arrays

---
+++ Median Equal Sorted Array/Median Equal Sorted Array/median_equal_sorted_array.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "median_equal_sorted_array.h"
+
+
+int median(int ar_1[], int ar_2[], int size_1)
+{
+	if(ar_1[size_1-1] < ar_2[0])
+		return ((ar_1[size_1 - 1] + ar_2[0]) / 2);
+
+	if (ar_2[size_1 - 1] < ar_1[0])
+		return ((ar_2[size_1 - 1] + ar_1[0]) / 2);
+
+	int i = 0, j = 0, k = 0, m1 = -1, m2 = -1;
+	while (k<size_1+1)
+	{
+		if (ar_1[i] <= ar_2[j])
+		{
+			m1 = m2;
+			m2 = ar_1[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			m1 = m2;
+			m2 = ar_2[j];
+			j++;
+			k++;
+		}
+	}
+	
+	return ((m1+m2) / 2);
+	
+}
---
+++ Median Equal Sorted Array/Median Equal Sorted Array/median_equal_sorted_array.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int median(int[], int[], int);
+
